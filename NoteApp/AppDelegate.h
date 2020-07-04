ac8a56524efe03e14f79674b88cd244de37c3e06
Ali Elsokary <ali.elsokary8@gmail.com>
2020-07-04 12:30:37 +0200

    Create Xcode Project

---
+++ NoteApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NoteApp
+//
+//  Created by Ali Elsokary on 7/4/20.
+//  Copyright © 2020 mag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NoteApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoteApp
+//
+//  Created by Ali Elsokary on 7/4/20.
+//  Copyright © 2020 mag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
